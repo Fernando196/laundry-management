978657ae495eb1b534ecc98d68b60d3f23239069
Fernando196 <fer_jose1996@hotmail.com>
2026-04-21 15:59:12 -0500

    fix: remove absolute footer in ConfirmModal for proper flex layout

--- app/components/common/Modal/ConfirmModal.vue
+++ app/components/common/Modal/ConfirmModal.vue
@@ -15,18 +15,16 @@
   const { close } = useModal()
 </script>
 <template>
-  <div class="relative flex h-auto w-auto flex-col bg-white text-black">
+  <div class="relative flex h-auto w-auto flex-col rounded-2xl bg-white text-black">
     <button class="absolute top-3 right-5 h-6 w-6 cursor-pointer" @click="$emit('close')">
       <MapIcon name="close" class="stroke-black" />
     </button>
 
-    <section class="mt-5 flex h-37.5 flex-col border-b border-gray-200 px-3 py-2 text-center">
+    <main class="flex flex-1 flex-col px-3 text-center">
       <h2 class="text-xl">{{ title }}</h2>
-      <span v-if="description" class="text-neutral-500">{{ description }}</span>
-    </section>
-    <footer
-      class="absolute bottom-0 flex w-full justify-end gap-3 border-t border-gray-200 px-3 py-4"
-    >
+      <span v-if="description" class="mt-4 text-neutral-500">{{ description }}</span>
+    </main>
+    <footer class="flex w-full justify-end gap-3 border-t border-t-gray-200 px-3 py-4">
       <button class="rounded-md bg-gray-200 px-5 py-1 text-gray-500" @click="close(false)">
         Cancelar
       </button>
