8bd36c80fdf0b90eb67562e29aa76f1f8511c312
Fernando196 <fer_jose1996@hotmail.com>
2026-04-24 18:24:40 -0500

    chore: add order_products resource to mock API

--- api/server.cjs
+++ api/server.cjs
@@ -58,6 +58,7 @@ server.listen(PORT, () => {
   console.log('  Recursos disponibles:')
   console.log('    GET/POST   http://localhost:' + PORT + '/machines')
   console.log('    GET/POST   http://localhost:' + PORT + '/orders')
+  console.log('    GET/POST   http://localhost:' + PORT + '/order_products')
   console.log('    GET/POST   http://localhost:' + PORT + '/users')
   console.log('    GET/POST   http://localhost:' + PORT + '/products')
   console.log('')
