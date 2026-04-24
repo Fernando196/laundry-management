8c9f086a93c72b965609cc298edaa0b6d33535bd
Fernando196 <fer_jose1996@hotmail.com>
2026-04-23 23:43:08 -0500

    refactor: migrate orders store to async API calls

--- app/store/orders.store.ts
+++ app/store/orders.store.ts
@@ -1,22 +1,41 @@
-import { ordersData } from '~/data/orders.data'
+import { ORDER_SERVICE_TYPE_CATALOG, ORDER_STATUS_CATALOG } from '~/const/orders.const'
+import { orderService } from '~/services/orders'
 import type { IOrder } from '~/types/order.type'
 
 export const useOrderStore = defineStore('orders', () => {
-  const orders = useState<IOrder[]>('orders', () => ordersData.slice(0, 10))
+  const orders = ref<IOrder[]>([])
+  const pending = ref<boolean>(false)
 
-  const addOrder = (order: IOrder) => {
-    if (!order.id) {
-      order.id = orders.value.length ? Math.max(...orders.value.map((o) => o.id!)) + 1 : 1
+  async function fetchOrders() {
+    pending.value = true
+    try {
+      const data: IOrder[] = <IOrder[]>await orderService.getOrders()
+      orders.value = data.map((order: IOrder) => ({
+        ...order,
+        Service: ORDER_SERVICE_TYPE_CATALOG[order.service] || null,
+        Status: ORDER_STATUS_CATALOG[order.status] || null,
+      }))
+      return data
+    } catch (error) {
+      console.error('Error fetching orders:', error)
+      throw error
+    } finally {
+      pending.value = false
     }
-
-    orders.value.push(order)
   }
 
-  const deleteOrder = (orderId: number) => {
+  const addOrder = async (order: IOrder) => {
+    const newOrder = await orderService.postOrder(order)
+    orders.value.push(newOrder)
+  }
+
+  const deleteOrder = async (orderId: number) => {
+    await orderService.deleteOrder(orderId)
     orders.value = orders.value.filter((order) => order.id !== orderId)
   }
 
-  const updateOrder = (updatedOrder: IOrder) => {
+  const updatedOrder = async (id: number, updated: IOrder) => {
+    const updatedOrder = await orderService.putOrder(id, updated)
     const index = orders.value.findIndex((order) => order.id === updatedOrder.id)
     if (index !== -1) {
       orders.value[index] = updatedOrder
@@ -30,6 +49,7 @@ export const useOrderStore = defineStore('orders', () => {
     // Actions
     addOrder,
     deleteOrder,
-    updateOrder,
+    updatedOrder,
+    fetchOrders,
   }
 })
