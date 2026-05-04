15d08246120cabc6ae733e94097b83e08721b923
Fernando196 <fer_jose1996@hotmail.com>
2026-05-03 23:36:44 -0500

    feat: add ProductForm modal component

---
+++ app/components/products/ProductForm.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+  import type { IProduct } from '~/types/products.type'
+  import CustomModal from '../common/Modal/CustomModal.vue'
+
+  interface Props {
+    product: IProduct
+  }
+
+  const props = defineProps<Props>()
+  const productForm = ref<IProduct>(
+    props.product || {
+      id: null,
+      name: '',
+      category: '',
+      brand: '',
+      unit: '',
+      stock: 0,
+      price: 0,
+      description: '',
+    }
+  )
+</script>
+<template>
+  <CustomModal :title="productForm?.id ? 'Editar' : 'Crear' + ' Producto'">
+    <div class="grid h-max w-full grid-cols-12 content-start gap-4 pb-10 md:w-150">
+      <InputWapper
+        id="product-form-modal-name"
+        v-model="productForm.name"
+        label=""
+        class="col-span-12 md:col-span-6"
+        type="text"
+      />
+      <InputWapper
+        id="product-form-modal-category"
+        v-model="productForm.category"
+        label=""
+        class="col-span-12 md:col-span-6"
+        type="text"
+      />
+      <InputWapper
+        id="product-form-modal-brand"
+        v-model="productForm.brand"
+        label=""
+        class="col-span-12 md:col-span-6"
+        type="text"
+      />
+      <InputWapper
+        id="product-form-modal-unit"
+        v-model="productForm.unit"
+        label=""
+        class="col-span-12 md:col-span-6"
+        type="text"
+      />
+      <InputWapper
+        id="product-form-modal-stock"
+        v-model="productForm.stock"
+        label=""
+        class="col-span-12 md:col-span-6"
+        type="text"
+      />
+      <InputWapper
+        id="product-form-modal-price"
+        v-model="productForm.price"
+        label=""
+        class="col-span-12 md:col-span-6"
+        type="text"
+      />
+      <InputWapper
+        id="product-form-modal-description"
+        v-model="productForm.description"
+        label=""
+        class="col-span-12 md:col-span-6"
+        type="text"
+      />
+    </div>
+  </CustomModal>
+</template>
